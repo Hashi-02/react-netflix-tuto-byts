da55c76cc5d13a33846db5f644f2683941347c57
hashi-02 <hassie0209@gmail.com>
2022-07-25 18:38:18 +0900

    画像表示

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,6 +1,8 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import instance from './axios';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import './Row.scss';
+
+const base_url = 'https://image.tmdb.org/t/p/original';
 
 type Props = {
   title: string;
@@ -17,19 +19,34 @@ type Movie = {
   backdrop_path: string;
 };
 
-export const Row = ({ title, fetchUrl }: Props) => {
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+
   useEffect(() => {
     async function fetchData() {
-      const request = await instance.get(fetchUrl);
+      const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
-
       return request;
     }
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
-
-  return <div className="Row" />;
+  return (
+    <div className="Row">
+      <h2>{title}</h2>
+      <div className="Row-posters">
+        {/* ポスターコンテンツ */}
+        {movies.map((movie, i) => (
+          <img
+            key={movie.id}
+            className={`Row-poster ${isLargeRow && 'Row-poster-large'}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
