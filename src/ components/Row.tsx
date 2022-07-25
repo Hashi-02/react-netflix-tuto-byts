17e79ad883b891492ec03684721ed2720fb9d729
hashi-02 <hassie0209@gmail.com>
2022-07-25 19:13:59 +0900

    componetsフォルダに移動

--- src/ components/Row.tsx
+++ src/ components/Row.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import axios from '../axios';
 import './Row.scss';
 
 const base_url = 'https://image.tmdb.org/t/p/original';
